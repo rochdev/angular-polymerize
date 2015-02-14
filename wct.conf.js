12bf89729621d6f52f4f490d8efe7432fb47d060
rochdev <roch.devost@gmail.com>
2015-02-14 17:41:10 -0500

    moved tests to SauceLabs

--- wct.conf.js
+++ wct.conf.js
@@ -2,7 +2,32 @@ module.exports = {
   verbose: false,
   plugins: {
     local: {
-      browsers: ['chrome', 'firefox']
+      disabled: true
+    },
+    sauce: {
+      browsers: [
+        {
+          name: 'Chrome',
+          base: 'SauceLabs',
+          browserName: 'chrome',
+          platform: 'Windows 8.1',
+          version: '40'
+        },
+        {
+          name: 'Firefox',
+          base: 'SauceLabs',
+          browserName: 'firefox',
+          platform: 'Windows 8.1',
+          version: '35'
+        },
+        {
+          name: 'Internet Explorer',
+          base: 'SauceLabs',
+          browserName: 'internet explorer',
+          platform: 'Windows 8.1',
+          version: '11'
+        }
+      ]
     }
   },
 };
